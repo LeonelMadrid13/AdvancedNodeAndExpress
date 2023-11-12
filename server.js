75382eba38153b64f1f3fa5fada522641c832f33
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-11-11 20:10:11 -0600

    fix: added error to deserializeUser callback

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ myDB(async client => {
   
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(null, doc);
+        done(error, doc);
       });
   });
 
