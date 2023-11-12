f76ff0d1cc4d8c892d9bf817b2a37610e7961bb8
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-11-11 20:22:18 -0600

    fix: myDB part

--- server.js
+++ server.js
@@ -28,24 +28,23 @@ app.set('views', './views/pug')
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  // Be sure to change the title
   app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
-    res.render('index', { title: 'Hello', message: 'Please log in' })
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in'
+    });
   });
 
-  // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
   
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(error, doc);
-      });
+      done(null, doc);
+    });
   });
 
-  // Be sure to add this...
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
